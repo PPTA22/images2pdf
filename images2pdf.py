671acf7c1d508c5d25f4ed0f32cb71522dfa7145
PPTA22 <40493117+PPTA22@users.noreply.github.com>
2020-07-16 05:18:52 -0700

    Add files via upload
    initial

---
+++ images2pdf.py
@@ -0,0 +1,13 @@
+import os
+from PIL import Image
+
+files = os.listdir()
+files.pop()
+print(files)
+
+print()
+images = []
+for f in files:
+    images.append(Image.open(f).convert('RGB'))
+
+images[0].save('all.pdf',save_all=True,append_images=images)
