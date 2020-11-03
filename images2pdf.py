80bea9c364d9490f5068f17003f7d59f655de670
PPTA22 <40493117+PPTA22@users.noreply.github.com>
2020-11-03 11:56:21 +0800

    Update images2pdf.py
    fix duplicated on 1st image

--- images2pdf.py
+++ images2pdf.py
@@ -10,4 +10,4 @@ images = []
 for f in files:
     images.append(Image.open(f).convert('RGB'))
 
-images[0].save('all.pdf',save_all=True,append_images=images)
+images[0].save('all.pdf',save_all=True,append_images=images[1:])
